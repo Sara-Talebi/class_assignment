377106d377532e821e70fad4fed573cfb0d780a8
Sara-Talebi <stalebi@g.syr.edu>
2024-09-04 19:48:15 -0400

    total force function completed

--- falling_cow_simulator.py
+++ falling_cow_simulator.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib
 
 ##-----------------------------------------------------------------##
@@ -12,20 +12,37 @@ def subfunction2():
     return 
     
 ##-----------------------------------------------------------------##
-def subfunction3():
-    print()
-    return 
+def tot_force_vec(velocity,mass,g,wind_res_cons):
+    f_grav = -mass * g
+    
+    v_x, v_y    = velocity
+    v_magnitude = np.sqrt(v_x**2 + v_y**2)
+    
+    f_wind_x = -wind_res_cons * v_x * v_magnitude
+    f_wind_y = -wind_res_cons * v_y * v_magnitude
+    
+    f_tot_x = f_wind_x
+    f_tot_y = f_grav_y + f_wind_y
+    f_tot   = np.array([f_tot_x,f_tot_y])
+
+    return f_tot
 
 ##-----------------------------------------------------------------##
 def test_function():
  	mass          = 1000 #kg
  	ini_pos_vec   = [0,100]
  	ini_vel_vec   = [10,10]
+ 	time	      = 0.0
  	dt            = 0.1
  	wind_res_cons = 0.1
  	g	      = 9.8 #m/s^2
  	
-    return nothing
+ 	position = np.array(ini_pos_vec)
+ 	velocity = np.array(ini_vel_vec)
+ 	
+ 	print(tot_force_vec(velocity,mass,g,wind_res_cons))
+
+
 ##-----------------------------------------------------------------##
 test_function()
 ##-----------------------------------------------------------------##
