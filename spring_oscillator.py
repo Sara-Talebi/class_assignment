473566845b3f6c2c47fc48e93fdef5005622180a
Sara-Talebi <stalebi@g.syr.edu>
2024-09-18 18:56:06 -0400

    changing for to while and coding symplectic func

--- spring_oscillator.py
+++ spring_oscillator.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 
 
 ##-----------------------------------------------------------------##
+def symplectic(m, k, x, v, dt):
+    new_v = v + (dt * (-(k/m) * x))
+    new_x = x + (dt * new_v)
+    return new_x, new_v
+##-----------------------------------------------------------------##
 def explicit(m, k, x, v, dt):
     new_x = x + (dt * v)
     new_v = v + (dt * (-(k/m) * x))
@@ -11,9 +16,9 @@ def explicit(m, k, x, v, dt):
 ##-----------------------------------------------------------------##
 def test_function():
     m  = 10 
-    k  = 0.1 
-    x0 = 1 
-    v0 = 0
+    k  = 0.9
+    x0 = 1.0
+    v0 = 0.0
     dt = 0.1
     t0 = 0 
     
@@ -24,17 +29,25 @@ def test_function():
     x.append(x0)
     v.append(v0)
     t.append(t0)
+    t_curr = t0
     
-    for i in range(0, 10):
-        x[i+1], v[i+1] = explicit(m, k, x[i], v[i], dt)
-        x.append(x[i+1])
-        v.append(v[i+1])
-        new_t = t[i] + dt
-        t.append(new_t)
-       
+    while t_curr <= 100:
+        x_last = x[-1]
+        v_last = v[-1]
+        #x_new, v_new = explicit(m, k, x_last, v_last, dt)
+        x_new, v_new = symplectic(m, k, x_last, v_last, dt)
+        x.append(x_new)
+        v.append(v_new)
+        t_curr += dt
+        t.append(t_curr)
         
+       
+    #print(x,'\n', t)    
     plt.figure()
     plt.plot(t, x)
+    plt.xlabel('time')
+    plt.ylabel('position')
+    plt.title('x vs t')
     plt.show()
     
 
