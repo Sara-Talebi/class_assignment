07e09a61dcb71eff43a36cdc5a7267bdc1d99d6d
Sara-Talebi <stalebi@g.syr.edu>
2024-09-18 19:53:53 -0400

    Runge-Kutta 2nd-order func added

--- spring_oscillator.py
+++ spring_oscillator.py
@@ -1,7 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+##-----------------------------------------------------------------##
+def RK_2nd(m, k, x, v, dt):
+    #k1 = f(tn,xn)
+    k1_x = v
+    k1_v = -(k/m) * x
+    
+    #k2 = xn + dtk1/2
+    k2_x = x + (0.5 * dt * k1_x)
+    k2_v = v + (0.5 * dt * k1_v)
+    
+    k3_x = x + (dt * k2_v)
+    
+    return k3_x
 ##-----------------------------------------------------------------##
 def symplectic(m, k, x, v, dt):
     new_v = v + (dt * (-(k/m) * x))
@@ -25,11 +37,13 @@ def test_function():
     x = []
     v = []
     t = []
+    RK_x = []
     
     x.append(x0)
     v.append(v0)
     t.append(t0)
     t_curr = t0
+    RK_x.append(x0)
     
     while t_curr <= 100:
         x_last = x[-1]
@@ -40,11 +54,12 @@ def test_function():
         v.append(v_new)
         t_curr += dt
         t.append(t_curr)
-        
+        k3_x = RK_2nd(m, k, x_last, v_last, dt)
+        RK_x.append(k3_x)
        
     #print(x,'\n', t)    
     plt.figure()
-    plt.plot(t, x)
+    plt.plot(t, RK_x)
     plt.xlabel('time')
     plt.ylabel('position')
     plt.title('x vs t')
